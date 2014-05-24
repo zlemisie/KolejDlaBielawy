80ef014914b0e6f75b3f097f7277342dacdf04dd
zlemisie <zlemisie@gmail.com>
2014-05-24 14:07:30 +0200

    original background color

--- ZombieBird/src/com/kilobolt/GameWorld/GameRenderer.java
+++ ZombieBird/src/com/kilobolt/GameWorld/GameRenderer.java
@@ -251,7 +251,7 @@ public class GameRenderer {
 		shapeRenderer.begin(ShapeType.Filled);
 
 		// Draw Background color
-//		shapeRenderer.setColor(55 / 255.0f, 80 / 255.0f, 100 / 255.0f, 1);
+		shapeRenderer.setColor(55 / 255.0f, 80 / 255.0f, 100 / 255.0f, 1);
 		shapeRenderer.rect(0, 0, 136, midPointY + 66);
 
 		// Draw Grass
